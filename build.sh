95063f6f4fa27dcd4555ec8f3e6459e1a7090da0
Marco Chiodo <marcochio94@gmail.com>
2025-11-21 10:05:37 +0100

    added php 8.5 with alpine 3.22

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@ then
 elif test "${1}" = "84";
 then
     docker buildx build --build-arg V=84 --build-arg ALPINE_VERSION=3.21 -t sigblue/nginx-php:84 --no-cache .
+elif test "${1}" = "85";
+then
+    docker buildx build --build-arg V=85 --build-arg ALPINE_VERSION=3.22 -t sigblue/nginx-php:85 --no-cache .
 else
     echo "Error: PHP version not valid";
     exit 2;
